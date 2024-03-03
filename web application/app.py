1c93af25c968db1f39c0558f6d62132bcdf431db
Satya Prakash Mohanty <mebubu03@gmail.com>
2024-03-03 11:11:55 +0530

    addin the unstage file to staging area

--- web application/app.py
+++ web application/app.py
@@ -46,5 +46,41 @@ def excel():
             img_title = "Histogram"
     return render_template('xlsx.html', table=table, head=head, img=img, head_title=head_title, table_title=table_title, img_title=img_title)
 
+@app.route("/csv", methods=['GET', 'POST'])
+def csv():
+    table = ""
+    head = ""
+    img = ""
+    head_title = ""
+    table_title = ""
+    img_title = ""
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file part'
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+        if file:
+            df = pd.read_csv(file)
+            table = df.describe().to_html()
+            head = df.head(5).to_html()
+            head_title = "First five Rows"
+            table_title = "Descriptive Statistics"
+
+            # Plot histogram
+            plt.figure(figsize=(8,6))
+            df.hist()
+            plt.tight_layout()  # Adjusts subplot params so that the subplot fits in the figure area
+
+            # Save it to a BytesIO object
+            buf = BytesIO()
+            plt.savefig(buf, format="png")
+
+            # Embed the result in the html output.
+            data = base64.b64encode(buf.getbuffer()).decode("ascii")
+            img = f"data:image/png;base64,{data}"
+            img_title = "Histogram"
+    return render_template('csv.html', table=table, head=head, img=img, head_title=head_title, table_title=table_title, img_title=img_title)
+
 if __name__=="__main__":
     app.run(debug=True, port=8000)
